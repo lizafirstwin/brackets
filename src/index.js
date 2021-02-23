908a2a016a0b9e77e3f8ce8ae5cb7c2cac1a5e9a
lizafirstwin <66125093+lizafirstwin@users.noreply.github.com>
2021-02-23 17:21:07 +0300

    fix: add functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-module.exports = function check(str, bracketsConfig) {
-	str = str.split('');
-  if(str.length % 2 != 0) return false;
-  return true;
-}
+module.exports = function function check(str, bracketsConfig) {
+  let brack = [];
+  for (const item of bracketsConfig) {
+    brack.push(item.join(''))
+  }
+  for (let i = 0; i < brack.length; i++) {
+    if (str.includes(brack[i])) {
+      str = str.replace(brack[i], '');
+      i = -1
+    }
+  }
+ return str.length === 0 ? true : false
+};
